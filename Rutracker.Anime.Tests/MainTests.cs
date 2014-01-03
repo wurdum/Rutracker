0340167240552d581e87135473c7c55104abd297
wurdum <pavel.ritikov@gmail.com>
2014-01-03 16:58:58 +0200

    several test cases were added for TitleParser

--- Rutracker.Anime.Tests/MainTests.cs
+++ Rutracker.Anime.Tests/MainTests.cs
@@ -29,10 +29,28 @@ namespace Rutracker.Anime.Tests
                     "Трусливый Велосипедист / Yowamushi Pedal (Набэсима Осаму) [TV] [01-09 из 12] [Без хардсаба] [RUS(int), JAP+SUB] [2013 г., спорт, HDTVRip] [720p]",
                     new Models.Anime {
                         Names = new[] { "Трусливый Велосипедист", "Yowamushi Pedal" },
-                        Types = new[] {Models.Anime.Type.TV },
+                        Types = new[] { Models.Anime.Type.TV },
                         Series = new Series(1, 9, 12),
                         Tracks = new[] { "RUS(int)", "JAP+SUB" },
                         Traits = new Traits(2013, new[] { "спорт" }, "HDTVRip")
+                    });
+                yield return new TestCaseData(
+                    "Прекрасные деньки / Деревенская глубинка / Глухомань / Non Non Biyori (Кавамо Синъя) [TV] [01-10 из 12] [Без хардсаба] [RUS(int), JAP+SUB] [2013 г., комедия, повседневность, HDTVRip] [720p]",
+                    new Models.Anime {
+                        Names = new[] { "Прекрасные деньки", "Деревенская глубинка", "Глухомань", "Non Non Biyori" },
+                        Types = new[] { Models.Anime.Type.TV },
+                        Series = new Series(1, 10, 12),
+                        Tracks = new[] { "RUS(int)", "JAP+SUB" },
+                        Traits = new Traits(2013, new[] { "комедия", "повседневность" }, "HDTVRip")
+                    });
+                yield return new TestCaseData(
+                    "Кейон!! [ТВ-2] / Кей-Он / K-On!! (Ямада Наоко) [TV+Special] [24+9 из 24+9] [без хардсабa] [RUS(ext), JAP+SUB] [2010 г., комедия, музыкальный, школа, BDRip] [1080p]",
+                    new Models.Anime {
+                        Names = new[] { "Кейон!!", "Кей-Он", "K-On!!" },
+                        Types = new[] { Models.Anime.Type.TV, Models.Anime.Type.Special },
+                        Series = new Series(24, 24),
+                        Tracks = new[] { "RUS(ext)", "JAP+SUB" },
+                        Traits = new Traits(2010, new[] { "комедия", "музыкальный", "школа" }, "BDRip")
                     });
             }
         }
